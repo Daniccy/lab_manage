2f5e9b636a5c29927a879b8a65e443b18bf8b25e
Daniccy <2581412437@qq.com>
2023-03-22 22:21:18 +0800

    首次提交

---
+++ src/main/java/com/example/lab/LabManageApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabManageApplication.class, args);
+	}
+
+}
